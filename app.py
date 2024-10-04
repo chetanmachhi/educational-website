f6232c9d5bff449292de5400b7b072ec143d0e63
chetanmachhi <157340124+chetanmachhi@users.noreply.github.com>
2024-10-04 03:17:29 +0000

    chanched some files

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
 
 @app.route("/", methods=["GET"])
 def main():
@@ -20,3 +22,13 @@ def english():
 @app.route("/maths", methods=["GET"])
 def maths():
     return render_template("maths.html") 
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
